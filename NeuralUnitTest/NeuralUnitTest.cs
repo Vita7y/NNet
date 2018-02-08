6f7006c493feae0377ccff559bd1017de3214f7f
vitallium <vitallium@yandex.ru>
2018-02-09 01:14:58 +0300

    Add 9 neuron network

--- NeuralUnitTest/NeuralUnitTest.cs
+++ NeuralUnitTest/NeuralUnitTest.cs
@@ -61,5 +61,40 @@ namespace NeuralUnitTest
             Assert.AreEqual(0.6457, res2);
         }
 
+        [TestMethod]
+        public void Test9NeuronInit()
+        {
+            var input1 = new Neuron(false);
+            var input2 = new Neuron(false);
+            var input3 = new Neuron(false);
+
+            var neuron21 = new Neuron();
+            var neuron22 = new Neuron();
+            var neuron23 = new Neuron();
+
+            var neuron31 = new Neuron();
+            var neuron32 = new Neuron();
+            var neuron33 = new Neuron();
+
+            input1.Connect(new Dictionary<Neuron, double>() { { neuron21, 0.9 }, { neuron22, 0.2 }, { neuron23, 0.1 } });
+            input2.Connect(new Dictionary<Neuron, double>() { { neuron21, 0.3 }, { neuron22, 0.8 }, { neuron23, 0.5 } });
+            input3.Connect(new Dictionary<Neuron, double>() { { neuron21, 0.4 }, { neuron22, 0.2 }, { neuron23, 0.6 } });
+
+            neuron21.Connect(new Dictionary<Neuron, double>() { { neuron31, 0.3 }, { neuron32, 0.6 }, { neuron33, 0.8 } });
+            neuron22.Connect(new Dictionary<Neuron, double>() { { neuron31, 0.7 }, { neuron32, 0.5 }, { neuron33, 0.1 } });
+            neuron23.Connect(new Dictionary<Neuron, double>() { { neuron31, 0.5 }, { neuron32, 0.2 }, { neuron33, 0.9 } });
+
+            input1.Activation(0.9);
+            input2.Activation(0.1);
+            input3.Activation(0.8);
+
+            var res1 = Math.Round(neuron31.ActivationValue, 3);
+            Assert.AreEqual(0.726, res1);
+            var res2 = Math.Round(neuron32.ActivationValue, 4);
+            Assert.AreEqual(0.7086, res2);
+            var res3 = Math.Round(neuron33.ActivationValue, 3);
+            Assert.AreEqual(0.778, res3);
+        }
+
     }
 }
